b626e2d35db008f439d150d64e32439486d761a1
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-05 19:38:39 -0300

    testing linking

---
+++ frontend/app/profile/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Profile() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>base para perfil, tiene pestañas: Reseñas, Ratings y Guardado</div>
+    </main>
+  );
+}
