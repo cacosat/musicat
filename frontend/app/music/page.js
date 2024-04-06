ea069248cb043d0052c3ce890d1326bc8dbac4a0
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-06 20:33:38 -0300

    root layout nav construction

--- frontend/app/music/page.js
+++ frontend/app/music/page.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Music() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex gap-2">
+        <Link href="/" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+            Go to app
+        </Link>
+      </div>
         <div>base para artista, canción y album</div>
     </main>
   );
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div className="flex gap-2">
+        <Link href="/music" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+            Go to Music
+        </Link>
+        <Link href="/profile" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+            Go to Profile
+        </Link>
+      </div>
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -38,15 +48,6 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
-
-      <div className="flex gap-2">
-        <Link href="/music" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-            Go to Music
-        </Link>
-        <Link href="/profile" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-            Go to Music
-        </Link>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
