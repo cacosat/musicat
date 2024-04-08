02a1c9d1650ae880b3fe4e7091eccaa24ffd381d
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-07 20:38:05 -0400

    layout work + color tweaks

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import SearchBar from "./components/search";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <div className="absolute inset-x-0 top-0 ml-12 my-8 flex justify-center">
+        <SearchBar name='barra de búsqueda'>
+          <Button variant="outline" type="submit">Subscribe</Button>
+          <Button variant="outline" type="submit">Subscribe</Button>
+        </SearchBar>
+      </div>
       <div className="flex gap-2">
         <Link href="/music" className="flex items-center justify-center px-4 py-2 border text-sm font-medium rounded-md text-white">
             Go to Music
