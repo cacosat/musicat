3626c2e0b786794c74606807d77a045de781e8dd
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-07 20:38:05 -0400

    layout work + color tweaks

--- frontend/app/components/Search.jsx
+++ frontend/app/components/Search.jsx
@@ -6,12 +6,11 @@ export default function SearchBar({children, ...props}) {
     const name = props.name;
 
     return <>
-        <div className="flex w-full max-w-sm items-center space-x-2">
+        <div className="flex items-center gap-2">
             <Input type="email" placeholder={`${name}`} />
-            {children}
-            {/* {children}, this should be the buttons to be added to the search bar according to each page */}
-            {/* <Button variant="outline" type="submit">Subscribe</Button>
-            <Button variant="outline" type="submit">Subscribe</Button> */}
+            <div className="flex gap-2">
+                {children}
+            </div>
         </div>
     </>
 }
--- frontend/components/ui/input.jsx
+++ frontend/components/ui/input.jsx
@@ -1,18 +1,26 @@
 import * as React from "react"
-
+import { ReactComponent as SearchIcon } from '@/assets/icons/search_icon.svg'
+import { searchIcon } from '@/assets/icons/search_icon.svg'
+import Image from "next/image"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+
   return (
-    (<input
-      type={type}
-      className={cn(
-        "flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
-        className
-      )}
-      ref={ref}
-      {...props} />
-      )
+    <div className="flex h-auto w-full rounded-md bg-lefter px-3 py-2 text-base">
+      <input
+        type={type}
+        className={cn(
+          "bg-lefter text-custom-gray-200 placeholder:text-custom-gray-300",
+          className
+        )}
+        ref={ref}
+        {...props} />
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M4 11C4 7.13401 7.13401 4 11 4C14.866 4 18 7.13401 18 11C18 12.8858 17.2543 14.5974 16.0417 15.8561C16.0073 15.8825 15.9743 15.9114 15.9428 15.9429C15.9113 15.9744 15.8824 16.0074 15.856 16.0418C14.5973 17.2543 12.8857 18 11 18C7.13401 18 4 14.866 4 11ZM16.6176 18.0319C15.078 19.2635 13.125 20 11 20C6.02944 20 2 15.9706 2 11C2 6.02944 6.02944 2 11 2C15.9706 2 20 6.02944 20 11C20 13.125 19.2635 15.0781 18.0319 16.6177L21.707 20.2929C22.0975 20.6834 22.0975 21.3166 21.707 21.7071C21.3165 22.0976 20.6833 22.0976 20.2928 21.7071L16.6176 18.0319Z" fill="#B3B3B3"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M4 11C4 7.13401 7.13401 4 11 4C14.866 4 18 7.13401 18 11C18 12.8858 17.2543 14.5974 16.0417 15.8561C16.0073 15.8825 15.9743 15.9114 15.9428 15.9429C15.9113 15.9744 15.8824 16.0074 15.856 16.0418C14.5973 17.2543 12.8857 18 11 18C7.13401 18 4 14.866 4 11ZM16.6176 18.0319C15.078 19.2635 13.125 20 11 20C6.02944 20 2 15.9706 2 11C2 6.02944 6.02944 2 11 2C15.9706 2 20 6.02944 20 11C20 13.125 19.2635 15.0781 18.0319 16.6177L21.707 20.2929C22.0975 20.6834 22.0975 21.3166 21.707 21.7071C21.3165 22.0976 20.6833 22.0976 20.2928 21.7071L16.6176 18.0319Z" fill="black" fill-opacity="0.2"/>
+        </svg>
+    </div>
   );
 })
 Input.displayName = "Input"
