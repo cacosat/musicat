391ec17a90781a531eb75a6aab20356591961d82
mviala10 <mviala10@gmail.com>
2024-04-11 16:22:00 +0100

    Profile Manuel (V1)

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -3,13 +3,37 @@ import Link from "next/link";
 
 export default function Profile() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="flex gap-2">
-          <Link href="/" className="flex items-center justify-center px-4 py-2 border text-sm font-medium rounded-md text-white">
-              Go to app
-          </Link>
+    <main className="flex flex-col min-h-screen w-full">
+        {/* Banner como un rectángulo que abarca todo el ancho superior */}
+        <div className="w-full h-64 bg-cards-light flex items-center justify-center">
+          {/* Puedes agregar una imagen o texto dentro de este div */}
+          <p className="text-2xl text-white font-bold">Bienvenido a Mi Aplicación</p>
         </div>
-        <div>base para perfil, tiene pestañas: Reseñas, Ratings y Guardado</div>
+
+       {/* Contenedor para los botones */}
+       <div className="w-full h-8 flex justify-center bg-lefter">
+          {/* Envoltorio de botones con ancho máximo y centrado */}
+          <div className="flex" style={{ maxWidth: '600px' }}>
+              <button className="w-1/3 h-full text-center text-white font-medium focus:outline-none bg-custom-gray-300 px-10">
+                  Reseñas
+              </button>
+              <button className="w-1/3 h-full text-center text-white font-medium focus:outline-none bg-custom-gray-400 px-10">
+                  Ratings
+              </button>
+              <button className="w-1/3 h-full text-center text-white font-medium focus:outline-none bg-custom-gray-500 px-10">
+                  Guardado
+              </button>
+          </div>
+        </div>
+
+        <div className="flex flex-1 flex-col items-center justify-center gap-4">
+          <div className="flex gap-2">
+            <Link href="/" className="flex items-center justify-center px-4 py-2 border text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">
+                Ir a la aplicación
+            </Link>
+          </div>
+          <div>Base para perfil, tiene pestañas: Reseñas, Ratings y Guardado</div>
+        </div>
     </main>
   );
 }
