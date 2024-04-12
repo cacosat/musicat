07040f69a46572b34e1aa259e3f844145e35e61e
mviala10 <mviala10@gmail.com>
2024-04-12 20:11:53 +0100

    Profile Manuel (V3)

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -5,47 +5,46 @@ import { useState } from 'react';
 export default function Profile() {
   // Agrega un nuevo estado para rastrear el botón activo
   const [activeTab, setActiveTab] = useState('reseñas');
-
   return (
-  <main className="flex flex-col min-h-screen w-full">
-        {/* Banner como un rectángulo que abarca todo el ancho superior */}
-        <div className="w-full h-64 bg-cards-light flex items-center justify-center">
-          {/* Puedes agregar una imagen o texto dentro de este div */}
-          <p className="text-2xl text-white font-bold">Bienvenido a Mi Aplicación</p>
+    <main className="flex flex-col min-h-screen w-full">
+      {/* Reduce el padding lateral para dar más espacio al contenido */}
+      <div className="flex items-center w-full h-72 bg-cards-light" style={{ padding: '0 15%' }}>
+        <div className="flex items-center justify-start w-1/3"> {/* Asegúrate de que el círculo esté alineado a la izquierda */}
+          <div className="w-56 h-56 bg-background rounded-full"></div>
         </div>
-
+        <div className="w-2/3 h-56 flex flex-col items-start justify-between pl-16"> {/* Añade un padding-left para alinear con el círculo */}
+          <h1 className="text-4xl text-white font-bold my-2">Ignacio Socias</h1>
+          {/* Aumenta el ancho del rectángulo blurry */}
+          <div className="w-full h-full bg-white/30 rounded-md border border-gray-300 my-2 backdrop-blur-md"></div>
+      </div>
+      </div> 
+      
        {/* Contenedor para los botones */}
        <div className="w-full h-8 flex justify-center bg-lefter">
           {/* Envoltorio de botones con ancho máximo y centrado */}
-          <div className="flex" style={{ maxWidth: '600px' }}>
+        <div className="flex" style={{ maxWidth: '600px' }}>
           <button 
-              onClick={() => setActiveTab('reseñas')}
-              className={`w-1/3 h-full text-center text-white font-medium focus:outline-none ${activeTab === 'reseñas' ? 'bg-background' : 'bg-lefter'} px-10`}
-            >
-                Reseñas
-            </button>
-            <button 
-              onClick={() => setActiveTab('ratings')}
-              className={`w-1/3 h-full text-center text-white font-medium focus:outline-none ${activeTab === 'ratings' ? 'bg-background' : 'bg-lefter'} px-10`}
-            >
-                Ratings
-            </button>
-            <button 
-              onClick={() => setActiveTab('guardado')}
-              className={`w-1/3 h-full text-center text-white font-medium focus:outline-none ${activeTab === 'guardado' ? 'bg-background' : 'bg-lefter'} px-10`}
-            >
-                Guardado
-            </button>
-          </div>
+            onClick={() => setActiveTab('reseñas')}
+            className={`w-1/3 h-full text-center ${activeTab === 'reseñas' ? 'text-white font-semibold bg-background' : 'text-custom-gray-200 font-light bg-lefter'} px-10 focus:outline-none`}
+          >
+            Reseñas
+          </button>
+          <button 
+            onClick={() => setActiveTab('ratings')}
+            className={`w-1/3 h-full text-center ${activeTab === 'ratings' ? 'text-white font-semibold bg-background' : 'text-custom-gray-200 font-light bg-lefter'} px-10 focus:outline-none`}
+          >
+            Ratings
+          </button>
+          <button 
+            onClick={() => setActiveTab('guardado')}
+            className={`w-1/3 h-full text-center ${activeTab === 'guardado' ? 'text-white font-semibold bg-background' : 'text-custom-gray-200 font-light bg-lefter'} px-10 focus:outline-none`}
+          >
+            Guardado
+          </button>
         </div>
-
-        <div className="flex flex-1 flex-col items-center justify-center gap-4">
-          <div className="flex gap-2">
-            <Link href="/" className="flex items-center justify-center px-4 py-2 border text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">
-                Ir a la aplicación
-            </Link>
-          </div>
-        </div>
-      </main>
+      </div>
+      
+</main>
   );
 }
+
