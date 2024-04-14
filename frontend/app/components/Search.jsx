67386ec40ff8fcfde3aaff27d48b9eea1ddc3963
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-14 00:23:32 -0400

    added the value of the input into a useState hook

--- frontend/app/components/Search.jsx
+++ frontend/app/components/Search.jsx
@@ -1,13 +1,26 @@
+'use client';
+
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
 
-export default function SearchBar({ children, fullWidth, ...props }) {
+export default function SearchBar({ children, ...props }) {
+    const [query, setQuery] = useState('');
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+    }
+
     const name = props.name;
 
     return (
         <div className="flex justify-center items-center gap-2 w-[80%]">
             <div className="rounded-md grow shadow-custom-vertical">
-                <Input type="email" placeholder={`${name}`} />
+                <Input type="email" 
+                    placeholder={`${name}`} 
+                    value={query}
+                    onChange={handleChange}
+                />
             </div>
             <div className="flex gap-2">
                 {children}
