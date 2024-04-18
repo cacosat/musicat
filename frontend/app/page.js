97ac1e1b1201a4007194c50381b64cf5d0acdb87
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-18 16:05:43 -0400

    profile revision

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -9,8 +9,8 @@ export default function Home() {
 
   return (
     <SearchProvider>
-      <SearchOverlay>
-          <main className="flex min-h-screen flex-col items-center gap-2 p-24">
+      <SearchOverlay searchBar={true}>
+          <main className="flex min-h-screen flex-col items-center gap-2 py-28">
             <div className="flex gap-2">
               <Link href="/music" className="flex items-center justify-center px-4 py-2 border text-sm font-medium rounded-md text-white">
                   Go to Music
@@ -29,7 +29,7 @@ export default function Home() {
               3. bg: se puede ingresar "url('/imagen.jpg')" (tiene que estar el archivo en carpeta public), 
               o se puede ingresar un color (solo acepta colores tailwind o los nuestros, ej.: 'red-500', 'cards')
             */}
-            <div className="w-56 h-56">
+            <div className="w-[80%] h-56">
               <GradientOutline bg="url('/Santiago.jpeg')" px='16' py='16'>
                 <div className="w-fit h-fit text-white">
                   <GradientOutline bg="lefter">
