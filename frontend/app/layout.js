4217982d8777384ac7dd19207a2636ad622e12d1
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-04-20 07:57:07 -0400

    search behaviour change, now clicking the icon opens the overlay

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import Navbar from "./components/nav";
+import SearchProvider from "./components/searchProvider";
 import { Button } from "@/components/ui/button";
 
 
@@ -17,12 +18,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-background`}>
         <div className="ml-[180px]"> {/* margin left equivalent to Navbar width */}
-          <div className="">
-            <Navbar />
-          </div>
-          <div>
-            {children}
-          </div>
+          <SearchProvider>
+            <div className="">
+              <Navbar />
+            </div>
+            <div>
+              {children}
+            </div>
+          </SearchProvider>
         </div>
       </body>
     </html>
