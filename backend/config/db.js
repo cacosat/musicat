6ebe0c6b9758837f224b7a8d6d1dd29268199fb7
mviala10 <mviala10@gmail.com>
2024-05-21 16:15:49 +0100

    Manuel Backend V2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
@@ -13,4 +13,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
