c2369ea746e8a6c08b180a392c5ef3f5abb88a2b
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-05-24 12:30:21 +0200

    mongodb testing

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({path: '../.env'});
 
 const uri = process.env.MONGO_URI;
 
@@ -12,16 +12,54 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
-const connectDB = async () => {
+                      
+async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1); // Detiene el proceso si no se puede conectar a la base de datos
-  }
-};
+      // Connect to the Atlas cluster
+        await client.connect();
 
-export default connectDB;
+        // Get the database and collection on which to run the operation
+        const db = client.db("sample_mflix");
+        const col = db.collection("users");
+
+        // Create new documents                                                                                                                                         
+        const userDocument = {
+          // "_id":{"$oid":"59b99db4cfa9a34dcd7885b6"},
+          "name":"test name 2",
+          "email":"test2@test.com",
+          "password":"test2_password"
+        }
+
+        // Insert the documents into the specified collection        
+        const p = await col.insertOne(userDocument);
+
+        // Find the document
+        const filter = { "name": "test name" };
+        const document = await col.findOne(filter);
+
+        // Print results
+        console.log("Document found:\n" + JSON.stringify(document));
+
+      } catch (err) {
+        console.log(err.stack);
+    }
+
+    finally {
+      await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// const connectDB = async () => {
+//   try {
+//     await client.connect();
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } catch (error) {
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1); // Detiene el proceso si no se puede conectar a la base de datos
+//   }
+// };
+
+// export default connectDB;
