3d47375e77965381b1bdd44c775674b3dc169e1e
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-05-27 13:08:09 +0200

    added debouncing to the search fetch

--- frontend/app/components/searchBox.jsx
+++ frontend/app/components/searchBox.jsx
@@ -7,27 +7,7 @@ import Image from "next/image";
 import { SearchQueryContext } from "../context";
 
 export default function SearchBar({ children, placeholder }) {
-    const {query, showSearchResults, handleQueryChange} = useContext(SearchQueryContext)
-    const search_type = 'artist'; // artist, album or song
-
-    useEffect(() => {
-        // TODO: fetch only on new chars, and apply debounce
-
-        const fetchData = async () => {
-            if (query.length > 0) {
-                try {
-                    const response_token = await fetch(`http://localhost:5000/api/spotify/spotify-search?query=${encodeURIComponent(query)}&type=${search_type}`);
-                    const search_data = await response_token.json();
-                    console.log(search_data);
-                } catch (error) {
-                    console.error('Error fetching token data: ', error);
-                }
-            }
-        };
-
-        fetchData();
-    }, [query]);
-
+    const {query, searchType, searchResults, showSearchResults, handleQueryChange} = useContext(SearchQueryContext)
 
     return (
         <div className="flex justify-center items-center gap-2 w-[80%]">
