7d2b22e4fa35fd4eb296413ddb6b251c7818369d
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-06-02 14:15:44 -0400

    start of list result items component

---
+++ frontend/app/components/searchResult.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GradientOutline from "./gradientOutline";
+
+export default function SearchResult() {
+
+    return <>
+        <GradientOutline>
+            <div className="text-white">
+                test
+            </div>
+        </GradientOutline>
+    </>
+}
