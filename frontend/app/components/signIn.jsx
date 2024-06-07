e912aa1e96d9ddb8f3595a9d2eaa860bd461ec48
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-06-06 21:15:38 -0400

    oauth work

--- frontend/app/components/signIn.jsx
+++ frontend/app/components/signIn.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import {handleSignIn} from "../../../backend/authActions.js"
-import { signIn } from "../../../backend/auth.js";
 
 export default function SignIn() {
-    const handleGoogleSignIn = async () => {
-        await signIn('google');
-    };
 
     return (
-        <form>
-            <button type="submit" >Signin with Google</button>
+        <form
+            action={handleSignIn}
+        >
+            <button type="submit">Signin with Google</button>
         </form>
     )
 }
